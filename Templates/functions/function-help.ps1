f5d634f47d91d878ae5f92d7eff578b25177a227
Richard Joubert <rjoubert@nsd.team>
2022-12-07 15:03:54 -0500

    help

---
+++ Templates/functions/function-help.ps1
@@ -0,0 +1,23 @@
+function New-CommentHelp {
+	[CmdletBinding()]
+	param(
+		$Parameter
+	)
+	Write-Host -Object "${Parameter}"
+	
+<#
+.SYNOPSIS
+	One-Sentence Summary.
+
+.DESCRIPTION
+	Short pararagraph summary.
+
+.NOTES
+	Extra Details.
+
+.LINK https://
+.LINK Related-Function
+.LINK Related-Cmdlet
+.LINK about_Related_Content
+#>
+}
