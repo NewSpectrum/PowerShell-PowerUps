dc50e4f3a1e53c583e2baba3c8624de36ff08dd2
Richard Joubert <rjoubert@nsd.team>
2022-12-07 19:18:43 -0500

    template updates

--- Templates/functions/function-help.ps1
+++ Templates/functions/function-help.ps1
@@ -1,9 +1,18 @@
 function New-CommentHelp {
 	[CmdletBinding()]
 	param(
-		$Parameter
+		$Name
+		$Date
+		$Pizza
 	)
-	Write-Host -Object "${Parameter}"
+	
+	if ( '$Pizza' -contains 'pizza' ) {
+		$PizzaFix = "$Pizza" -replace "(`s?Pizza)|(`s?pizza)"
+		$Pizza = "$PizzaFix"
+	}
+	
+	Write-Host -Object "${Name} had ${Pizza} Pizza on ${Date}"
+	
 	
 <#
 .SYNOPSIS
@@ -12,8 +21,47 @@ function New-CommentHelp {
 .DESCRIPTION
 	Short pararagraph summary.
 
+.PARAMETER Name
+	$Name Description
+
+.PARAMETER Date
+	$Date Description
+
+.PARAMETER Pizza
+	$Pizza Description
+
 .NOTES
 	Extra Details.
+
+.EXAMPLE 
+	New-CommentHelp -Name 'Ferdinand' -Date '02/01/1531' -Pizza 'Pepperoni'
+	[ details ]
+	[ simulated output ]
+
+.EXAMPLE 
+	New-CommentHelp -Example
+	[ details ]
+	[ simulated output ]
+
+.ROLE
+	[ name of the 'user role' for the help topic ]
+	(optional)
+
+.FUNCTIONALITY
+	[ keyword(s) describing the intended function ]
+	(optional)
+
+.COMPONENT
+	(optional)
+
+.INPUTS
+	(optional)
+
+.OUTPUTS
+	(optional)
+
+.FORWARDHELPCATEGORY Function
+.FORWARDHELPTARGETNAME Command-Name (optional)
 
 .LINK https://
 .LINK Related-Function
